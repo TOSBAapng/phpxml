fabc15561bb99642315e1b721f19d4f23b539621
TOSBAapng <73078721+TOSBAapng@users.noreply.github.com>
2024-04-17 17:12:05 +0300

    Update rssfeedphpxml.php

--- rssfeedphpxml.php
+++ rssfeedphpxml.php
@@ -30,3 +30,37 @@ foreach ($xml->channel->item as $item) {
 echo "</channel>
 </rss>";
 ?>
+
+
+
+
+
+<?php
+// RSS dosyasının URL'si
+$url = "http://aksehirportal.rf.gd/rss/category/Son";
+
+// RSS içeriğini çek
+$xml = simplexml_load_file($url);
+
+// XML dosyasını oluştur
+$netXML = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"><channel></channel></rss>');
+
+// RSS içeriğini döngüye alarak XML dosyasına ekle
+foreach ($xml->channel->item as $item) {
+    $newItem = $netXML->channel->addChild('item');
+    $newItem->addChild('title', htmlspecialchars($item->title));
+    $newItem->addChild('description', htmlspecialchars($item->description));
+    $newItem->addChild('link', htmlspecialchars($item->link));
+    $newItem->addChild('pubDate', htmlspecialchars($item->pubDate));
+}
+
+// XML dosyasını kaydet
+$netXML->asXML('net.xml');
+
+// XML başlığını belirle
+header('Content-type: application/rss+xml');
+
+// RSS başlığını yazdır
+echo $netXML->asXML();
+?>
+
